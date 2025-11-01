0c0f701583728e81b19c00d20d81aa3fb214b635
Anner246 <annerarevalo246@gmail.com>
2025-11-01 00:29:31 -0400

    Update Character.py
    Added reset method

--- Simple_RPG/Character.py
+++ Simple_RPG/Character.py
@@ -6,23 +6,46 @@ class Character:
         # --- Status Variables ---
         self.name = "PAWN"
         self.title = "Red Archon"
-        self.hp = 50
-        self.mp = 50
+        self.hp = 4
+        self.mp = 40
         self.max_hp = 50
         self.max_mp = 50
         self.atk = 12
-        self.defense = 9
+        self.defense = 8
         self.spd = 10
         
         self.equipment = {"Armor": ("Basic Armor", 0,),
                           "Weapon": ("Basic Weapon", 0)
                          }
         
-        self.items = {"HP Potion": ("HP Potion", 0), 
-                      "MP Potion": ("MP Potion", 0)
+        self.items = {"HP Potion": ("HP Potion", 1),
+                      "MP Potion": ("MP Potion", 1)
                      }
         
     def pickup(self, name:str):
         item_name, amount = self.items[name]   
-        self.items[name] = (item_name, amount+1) 
+        self.items[name] = (item_name, amount+1)
+        
+    def reset(self):
+        self.currentPosition = "Start"
+        
+        # --- Status Variables ---
+        self.hp = 4
+        self.mp = 40
+        self.max_hp = 50
+        self.max_mp = 50
+        self.atk = 12
+        self.defense = 8
+        self.spd = 10
 
+        # --- Reset equipment ---
+        self.equipment = {
+            "Armor": ("Basic Armor", 0),
+            "Weapon": ("Basic Weapon", 0)
+        }
+
+        # --- Reset items ---
+        self.items = {
+            "HP Potion": ("HP Potion", 1),
+            "MP Potion": ("MP Potion", 1)
+        }
